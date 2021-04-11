fc28a9a733718cdbf09863c8799d2a7c3388a287
StroveLight <pharaneda@uc.cl>
2021-04-11 19:41:39 -0400

    worker separado

--- src/crtree/main.c
+++ src/crtree/main.c
@@ -22,7 +22,7 @@ InputFile* data_in;
 FILE *output_file;
 
 void sig_int_handler(int);
-void sig_abrt_handler(int);
+void sig_abrt_handler(int), create_worker();
 
 int pid_main, pid_child, id, status, len_args, interrupted_child=0;
 time_t turnaround;
@@ -54,43 +54,7 @@ int main(int argc, char **argv)
   printf("ptype: %d\n", ptype_this);
   // hacer acciones necesarias
   if (ptype_this == W){
-    pid_child = fork();
-    len_args = atoi(data_in->lines[id][2]);
-    if (pid_child == 0){
-      signal(SIGINT, SIG_IGN);
-      char** command = calloc(len_args + 2, sizeof(char*));
-      command[0] = data_in->lines[id][1];
-      printf("args: %s\n", command[0]);
-      for (int i = 0; i < len_args; i++)
-      {
-        command[i + 1] = data_in->lines[id][3+i];
-      }
-      command[len_args + 1] = (char*)NULL;
-      execvp(command[0], command);
-    }
-    else{
-      turnaround = time(NULL);
-      pid_t wpid = waitpid(pid_child, &status, WUNTRACED);
-      turnaround = time(NULL) - turnaround;
-
-      if (WTERMSIG(status) | WSTOPSIG(status)) printf("Bad ending\n");
-      else printf("Good ending\n");
-
-      char* file_name = malloc(sizeof(char) * (10 + 6));
-      sprintf(file_name, "%i.txt", id);
-      output_file = fopen(file_name, "w");
-      fprintf(output_file, "%s,", data_in->lines[id][1]);
-      for (int i = 0; i < len_args; i++)
-      {
-        fprintf(output_file, "%s,", data_in->lines[id][3+i]);
-      }
-      fprintf(output_file, "%ld,%i,%i\n", turnaround, status, interrupted_child);
-      fclose(output_file);
-      free(file_name);
-      input_file_destroy(data_in);
-      exit(0);
-    }
-    
+    create_worker();
   }else{
     // crear timeout
   }
@@ -118,7 +82,46 @@ void sig_int_handler(int signum){
   if (ptype_this == R){
     // pasa error a los hijos como sigabort
   }
+  // else{
+  //   kill(pid_main,SIGABRT);
+  // }
+}
+
+void create_worker(){
+  pid_child = fork();
+  len_args = atoi(data_in->lines[id][2]);
+  if (pid_child == 0){
+    signal(SIGINT, SIG_IGN);
+    char** command = calloc(len_args + 2, sizeof(char*));
+    command[0] = data_in->lines[id][1];
+    printf("args: %s\n", command[0]);
+    for (int i = 0; i < len_args; i++)
+    {
+      command[i + 1] = data_in->lines[id][3+i];
+    }
+    command[len_args + 1] = (char*)NULL;
+    execvp(command[0], command);
+  }
   else{
-    kill(pid_main,SIGABRT);
+    turnaround = time(NULL);
+    pid_t wpid = waitpid(pid_child, &status, WUNTRACED);
+    turnaround = time(NULL) - turnaround;
+
+    if (WTERMSIG(status) | WSTOPSIG(status)) printf("Bad ending\n");
+    else printf("Good ending\n");
+
+    char* file_name = malloc(sizeof(char) * (10 + 6));
+    sprintf(file_name, "%i.txt", id);
+    output_file = fopen(file_name, "w");
+    fprintf(output_file, "%s,", data_in->lines[id][1]);
+    for (int i = 0; i < len_args; i++)
+    {
+      fprintf(output_file, "%s,", data_in->lines[id][3+i]);
+    }
+    fprintf(output_file, "%ld,%i,%i\n", turnaround, status, interrupted_child);
+    fclose(output_file);
+    free(file_name);
+    input_file_destroy(data_in);
+    exit(0);
   }
 }
